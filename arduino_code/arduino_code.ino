66efac89e82f56bc134d0e0dfddbdbb91f9a8b86
Mr-Moody <118393511+Mr-Moody@users.noreply.github.com>
2025-05-08 22:22:11 +0100

    movement logic and wall following

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -3,19 +3,29 @@
 #include <Wire.h>
 #include <SPI.h>
 #include <WiFi.h>
+#include <WiFiUdp.h>
 
 #include "constants.h"
 
+//initialise classes
 MotoronI2C mdL;
 
 //MotoronI2C mdL(0x10);
 //MotoronI2C mdR(0xF);
 
-Servo arm_servo;
+Servo sensor_servo;
 
-const int BASE_SPEED = 255; //base speed of robot in PWM
-int left_speed = 0;
-int right_speed = 0;
+WiFiUDP Udp; //UDP to receive WiFi signal
+
+
+int left_speed = 0; //percentage for left speed -100% to 100%
+int right_speed = 0; //percentage for right speed -100% to 100%
+
+//pwms for speed (need 4 as back wheels use different motors)
+int front_left_speed = 0;
+int front_right_speed = 0;
+int back_left_speed = 0;
+int back_right_speed = 0;
 
 //distance sensor distances
 float left_distance = 0.0f;
@@ -27,22 +37,51 @@ float floor_distance = 0.0f;
 char ssid[] = SECRET_SSID;        // your network SSID (name)
 char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
 int status = WL_IDLE_STATUS;     // the WiFi radio's status
+unsigned int localPort = 2390; // local port to listen for UDP packets
+
+const int PACKET_SIZE = 48;
+byte packetBuffer[PACKET_SIZE]; // buffer to hold incoming and outgoing packets
+
+
+bool button_pressed = false;
+
+//section logic
+int section_idx = 0;
+Section current_section = section_order[section_idx];
 
 
 // ---------- Sensor Reading Functions ----------
 
+/// @brief measure distance to wall in mm
+/// @param pin the digital pin which the sensor is connected to
+/// @return distance in mm
 float read_distance_sensor(int pin){
-
-  //read distance
-  float distance = 0.0f; // (temp value for now until we implement specific sensors)
-
+  float distance = 0.0f;
+  float total = 0.0f;
+  
   //read multiple times and take average to reduce noise
+  for (int i = 0; i < NUM_SENSOR_AVG){
+    distance = 0.0f; //implement actual logic to read sensor here
 
-  return distance;
+    total = total + distance;
+  }
+
+
+  return total / NUM_SENSOR_AVG;
 }
 
 
 // ---------- Util Functions ----------
+
+int read_udp(){
+  if (!Udp.parsePacket()){
+    return 0;
+  } 
+
+  Udp.read(packetBuffer, NTP_PACKET_SIZE);
+
+  word(packetBuffer)
+}
 
 void check_kill_switch(){
   //physical switch
@@ -50,42 +89,81 @@ void check_kill_switch(){
     shutdown();
   }
 
-  //implement wifi switch here
+  //wifi switch
+  if (read_udp()){
+    shutdown();
+  }
+  
 }
 
 void shutdown(){
   move(0,0); //stop robot movement
 
+  bool stopped = true;
+
   //force get stuck in loop to stop all movement
-  while (true){
+  while (stopped){
     Serial.println("Kill switch activated!");
     delay(1000);
+
+    //if switch detected again, continue operation
+    if (digitalRead(KILL_SWITCH)){
+      stopped = false;
+    }
   }
 }
 
-
+/// @brief sets the motors to the correct PWMs
+/// @param left percentage speed for left motors
+/// @param right percentage speed for right motors
 void move(int left, int right){
+  //capping percentages
+  if (left > 100){
+    left = 100
+  } else if (left < -100){
+    left = -100;
+  }
+
+  if (right > 100){
+    right = 100
+  } else if (right < -100){
+    right = -100;
+  }
+
   //implement setting the PWMs for all 6 wheels
 
-  // mdL.setSpeed(1, left);
-  // mdL.setSpeed(2, left);
-  // mdL.setSpeed(3, left);
+  front_left_speed = round(left / 100 * MAX_FRONT_PWM);
+  front_right_speed = round(right / 100 * MAX_FRONT_PWM);
+  back_left_speed = round(left / 100 * MAX_BACK_PWM);
+  back_right_speed = round(right / 100 * MAX_BACK_PWM);
 
-  // mdR.setSpeed(1, right);
-  // mdR.setSpeed(2, right);
-  // mdR.setSpeed(3, right);
+  // mdL.setSpeed(1, front_left_speed);
+  // mdL.setSpeed(2, front_left_speed);
+  // mdL.setSpeed(3, back_left_speed);
+
+  // mdR.setSpeed(1, front_right_speed);
+  // mdR.setSpeed(2, front_right_speed);
+  // mdR.setSpeed(3, back_right_speed);
 }
 
 
-void line_following(){
-  //implement logic here
+bool line_following(){
+  bool following = true;
 
+  while (following){
+    //tim tim please implement :)
+
+    check_kill_switch();
+  }
+
+  //only return true once the section has been completed
+  return true;
 }
 
 
-void wall_following(){
-  int left_speed = BASE_SPEED;
-  int right_speed = BASE_SPEED;
+bool wall_following(){
+  left_speed = BASE_SPEED;
+  right_speed = BASE_SPEED;
 
   bool following = true;
 
@@ -94,9 +172,27 @@ void wall_following(){
     right_distance = read_distance_sensor(RIGHT_DISTANCE_PIN);
     front_distance = read_distance_sensor(FRONT_DISTANCE_PIN);
 
+    //change to PID controller if needed
+    if (left_distance - right_distance > 10){
+      left_speed = left_speed - 1;
+      right_speed = right_speed + 1;
+    }
+    else if (right_distance - left_distance > 10){
+      left_speed = left_speed + 1;
+      right_speed = right_speed - 1;
+    }
 
-    //implement logic here
+    if (front_distance < 50){
+      move(0,0);
+    } else {
+      move(left_speed, right_speed);
+    }
+
+    check_kill_switch();
+    delay(50);
   }
+
+  return true;
 }
 
 
@@ -150,6 +246,8 @@ void setup(){
   printCurrentNet();
   printWifiData();
 
+  Udp.begin(localPort);
+
   //initialise all sensors here:
   // Serial.println("Initialising Left Motoron Motor Driver...");
   // mdL.reinitialize();
@@ -164,36 +262,39 @@ void setup(){
   // Serial.println("Right Motoron Motor Driver Connected!");
 
   // Serial.println("Attaching Servos!");
-  // arm_servo.attach(2);
+  // sensor_servo.attach(2);
 
   Serial.println("Initialisation completed!");
 
   delay(100);
+  
+  while(!button_pressed){
+    button_pressed = digitalRead(KILL_SWITCH);
+  }
 }
 
 void loop(){
-  // Serial.println("On!");
-  // mdL.setSpeed(1, 32000);
-  // delay(2000);
+  bool finished = false;
 
-  // Serial.println("Off!");
-  // mdL.setSpeed(1, 0);
-  // delay(2000);
+  switch (current_section)
+  {
+    case LINE_FOLLOW:
+      finished = line_following();
+      break;
 
-  // for (int pos = 0; pos <= 180; pos += 1) { // Move the servo from 0 to 180 degrees
-  //   arm_servo.write(pos); // Set the servo position
-  //   delay(15); // Wait for 15 milliseconds
-  // }
-  
-  // for (int pos = 180; pos >= 0; pos -= 1) { // Move the servo from 180 to 0 degrees
-  //   arm_servo.write(pos);
-  //   delay(15);
-  // }
+    case WALL_FOLLOW:
+      finished = wall_following();
+      break;
+    
+    default:
+      break;
+  }
 
+  if (finished){
+    section_idx++;
 
-  //implement stuff here idk
-  //layout order of sections when we know and testing stuff
+    current_section = section_order[section_idx];
+  }
 
   check_kill_switch();
-
 }
--- arduino_code/constants.h
+++ arduino_code/constants.h
@@ -9,3 +9,20 @@
 
 #define SECRET_SSID "yournetwork"
 #define SECRET_PASS "yourpassword"
+
+enum Section{
+    LINE_FOLLOW,
+    WALL_FOLLOW,
+    LAVA_FLOOR,
+    LUNAR_SURFACE,
+    GIANTS_CAUSEWAY,
+    ZIPLINE,
+};
+
+const Section section_order[] = [LINE_FOLLOW];
+
+const int BASE_SPEED = 50; //base speed of robot as percentage
+
+const int MAX_FRONT_PWM = 800;
+const int MAX_BACK_PWM = 800;
+const int NUM_SENSOR_AVG = 5; //number of times to read average of distance sensor to reduce noise
